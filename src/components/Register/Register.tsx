74de49cb84333c83858eb462f18b64908b3289f0
Mario Caballero <mario.caballero.948@gmail.com>
2023-05-01 14:12:58 -0300

    fix imports that never uses

--- src/components/Register/Register.tsx
+++ src/components/Register/Register.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import RegisterDisplay from "./RegisterDisplay";
 
 export default function Register() {
--- src/components/Register/RegisterDisplay.tsx
+++ src/components/Register/RegisterDisplay.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Button, Checkbox, Form, Input } from "antd";
+import { Button, Form, Input } from "antd";
 import { useNavigate } from "react-router";
 
 const RegisterDisplay: React.FC = () => {
