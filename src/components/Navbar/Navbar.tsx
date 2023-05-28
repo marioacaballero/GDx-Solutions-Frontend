e425d6eef960cd8ffccb43923fb884d68a4b47e6
Mario Caballero <mario.caballero.948@gmail.com>
2023-05-27 21:44:21 -0300

    hotfix nav

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,28 +1,11 @@
 import style from "./Navbar.module.css";
 import peruFlag from "../../assets/images/peruFlag.png";
-import { useNavigate } from "react-router-dom";
 
 export default function Navbar() {
-  const navigate = useNavigate();
-  const goTo = (path: string) => {
-    navigate(path);
-  };
   return (
     <nav className={style.navbar}>
-      <span
-        onClick={() => {
-          goTo("/");
-        }}
-      >
-        Quienes Somos
-      </span>
-      <span
-        onClick={() => {
-          goTo("/services");
-        }}
-      >
-        Servicios
-      </span>
+      <a href={`${import.meta.env.BASE_URL}`}>Quienes Somos</a>
+      <a href={`${import.meta.env.BASE_URL}services`}>Servicios</a>
       {/* <a href={"/singin"}>Ingresar</a> */}
       <div>
         <img src={peruFlag} alt="flag" />
