900adcc8bd8dd311014021f00d781598034480f8
Mario Caballero <mario.caballero.948@gmail.com>
2023-05-27 21:27:08 -0300

    fix navbar link

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,11 +1,28 @@
 import style from "./Navbar.module.css";
 import peruFlag from "../../assets/images/peruFlag.png";
+import { useNavigate } from "react-router-dom";
 
 export default function Navbar() {
+  const navigate = useNavigate();
+  const goTo = (path: string) => {
+    navigate(path);
+  };
   return (
     <nav className={style.navbar}>
-      <a href={"/"}>Quienes Somos</a>
-      <a href={"/services"}>Servicios</a>
+      <span
+        onClick={() => {
+          goTo("/");
+        }}
+      >
+        Quienes Somos
+      </span>
+      <span
+        onClick={() => {
+          goTo("/services");
+        }}
+      >
+        Servicios
+      </span>
       {/* <a href={"/singin"}>Ingresar</a> */}
       <div>
         <img src={peruFlag} alt="flag" />
