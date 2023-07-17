a07168079b18a1a7b6d87729a3f154e6b6da2d57
Mario Caballero <mario.caballero.948@gmail.com>
2023-07-17 20:52:11 -0300

    remove calendar library and add native input calendar

--- src/components/DataManagment/Control Panel/Calendar/Calendar.tsx
+++ src/components/DataManagment/Control Panel/Calendar/Calendar.tsx
@@ -1,7 +1,11 @@
-import FullCalendar from "@fullcalendar/react"; // must go before plugins
-import dayGridPlugin from "@fullcalendar/daygrid"; // a plugin!
-import interactionPlugin from "@fullcalendar/interaction"; // needed for dayClick
-import esLocale from "@fullcalendar/core/locales/es";
+import { useState } from "react";
+import swal from "sweetalert";
+import { BsSearch } from "react-icons/bs";
+import {
+  actualDateDay,
+  actualDateMonth,
+  actualDateYear,
+} from "../../../../assets/constants/initialStates";
 import style from "./Calendar.module.css";
 
 const CalendarGDx = ({
@@ -17,59 +21,47 @@ const CalendarGDx = ({
   setDay: React.Dispatch<React.SetStateAction<string>>;
   currentDate: string;
 }) => {
-  const handleDateClick = (arg: any) => {
-    if (currentDate != arg.dateStr) {
-      const dateArr = arg.dateStr.split("-");
-      setLoading(true);
-      setYear(dateArr[0]);
-      setMonth(dateArr[1]);
-      setDay(dateArr[2]);
+  const [input, setInput] = useState<string>(currentDate);
+  const handleDateClick = (e: React.ChangeEvent<HTMLInputElement>) => {
+    e.preventDefault();
+    if (!e.target.value) {
+      swal({
+        title: "Cuidado!",
+        text: "Debe seleccionar una fecha",
+        icon: "warning",
+      });
+      setInput(`${actualDateYear}-${actualDateMonth()}-${actualDateDay()}`);
+    } else if (currentDate != e.target.value) {
+      setInput(e.target.value);
     }
   };
 
-  const getCalendarHeight = () => {
-    const weekCount = 6;
-    const weekHeight = 26;
-    return weekCount * weekHeight;
-  };
-
-  const getCurrentDayCellClassNames = ({ date }: { date: Date }) => {
-    const classNames: string[] = [];
-
-    if (date.getDate() === new Date().getDate()) {
-      classNames.push("current-day");
-    }
-
-    return classNames;
-  };
-
-  const customDayCellContent = (args: any) => {
-    return (
-      <>
-        <div className="fc-daygrid-day-number">{args.dayNumberText}</div>
-      </>
-    );
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const dateArr = input.split("-");
+    setLoading(true);
+    setYear(dateArr[0]);
+    setMonth(dateArr[1]);
+    setDay(dateArr[2]);
   };
 
   return (
-    <div
-      style={{
-        width: "160px",
-        height: `${getCalendarHeight()}px`,
-        margin: "auto",
-      }}
-      className={style.calendar}
-    >
-      <FullCalendar
-        plugins={[dayGridPlugin, interactionPlugin]}
-        initialView="dayGridMonth"
-        nowIndicator
-        dayCellClassNames={getCurrentDayCellClassNames}
-        dateClick={handleDateClick}
-        locales={[esLocale]}
-        locale="es"
-        dayCellContent={customDayCellContent}
-      />
+    <div className={style.calendar}>
+      <form onSubmit={(e) => handleSubmit(e)}>
+        <input
+          type="date"
+          id="fecha"
+          name="calendar"
+          value={input}
+          min="2021-01-01"
+          max={`${actualDateYear}-${actualDateMonth()}-${actualDateDay()}`}
+          onChange={(e) => handleDateClick(e)}
+          lang="es"
+        />
+        <button type="submit">
+          <BsSearch />
+        </button>
+      </form>
     </div>
   );
 };
